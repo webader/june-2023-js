7b981a5cb924678867873157bc62d5dc4343009c
webader <alexey.derenko@gmail.com>
2023-08-20 21:39:51 +0300

    complete homework lesson2

--- lesson2/main.js
+++ lesson2/main.js
@@ -149,7 +149,6 @@ switch (weekOfTheDay) {
         console.log('Put in correct number day of the week');
 };
 
-
 //     - Користувач вводить або має два числа.
 let oneNumber = 1, twoNumber = 2;
 
@@ -159,15 +158,35 @@ if (oneNumber > twoNumber) {
 } else if (oneNumber < twoNumber) {
     console.log('oneNumber less then twoNumber', twoNumber);
 } else if (oneNumber === twoNumber) {
+    //         Також потрібно врахувати коли введені рівні числа.
     console.log('oneNumber equally then twoNumber');
 };
-//         Також потрібно врахувати коли введені рівні числа.
 
 //     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 //         за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
+let x;
+if (x === false || x === undefined || x ==='' || x===null || x === 0 ) {
+    console.log(x = 'default');
+} else {
+    console.log('x is not falsy');
+}
 
+//     - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray.
+//     За допомоги іф перевірити кожен його елемент на тривалість навчання.
+//     У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
 
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 
-//
-//
-//     - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+if (coursesAndDurationArray[0].monthDuration > 5) {console.log('Super')}
+if (coursesAndDurationArray[1].monthDuration > 5) {console.log('Super')}
+if (coursesAndDurationArray[2].monthDuration > 5) {console.log('Super')}
+if (coursesAndDurationArray[3].monthDuration > 5) {console.log('Super')}
+if (coursesAndDurationArray[4].monthDuration > 5) {console.log('Super')}
+if (coursesAndDurationArray[5].monthDuration > 5) {console.log('Super')}
